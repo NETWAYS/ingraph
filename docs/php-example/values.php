6a70326b1ec411ee9ffb67227a494277e1e8e281
Gunnar Beutner <gunnar.beutner@netways.de>
2011-02-04 16:08:35 +0100

    PHP example scripts.

---
+++ docs/php-example/values.php
@@ -0,0 +1,52 @@
+<?php
+
+$et = time() - 24*3600 * 0;// * 10.8;
+$st = time() - 24*3600 * 32;
+$dt = ($et - $st) / 300;
+
+$host = $_GET['host'];
+$parent_service = $_GET['parent_service'];
+$service = $_GET['service'];
+$plot = $_GET['plot'];
+
+$request = xmlrpc_encode_request("getPlotValues", array($host, $parent_service, $service, $plot, $st, $et, $dt, false));
+$context = stream_context_create(array('http' => array(
+	'method' => "POST",
+	'header' => "Content-Type: text/xml",
+	'content' => $request
+)));
+$file = file_get_contents("http://grapher:changeme@127.0.0.1:5000/", false, $context);
+$response = xmlrpc_decode($file);
+if ($response && xmlrpc_is_fault($response)) {
+	trigger_error("xmlrpc: $response[faultString] ($response[faultCode])");
+} else {
+	echo "var data = [\n";
+	
+	foreach (array('upper_limit', 'max', 'avg', 'min', 'lower_limit') as $type) {
+		echo <<<PREP
+		{
+			label: "$type",
+			data: [
+PREP;
+
+		ksort($response);
+		foreach ($response as $key => $val) {
+			$ts = (int)$key * 1000;
+			if (isset($val[$type])) {
+				$v = $val[$type];
+			} else {
+				$v = 'null';
+			}
+
+			echo "[ {$ts}, {$v} ],\n";
+		}
+		
+		echo <<<POST
+			]
+		},
+POST;
+	}
+
+	echo "];\n\n";
+}
+?>

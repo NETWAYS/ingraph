695bb028bae6f25707fa2c12e418a936144cb521
Eric Lippmann <eric.lippmann@netways.de>
2011-10-12 15:04:40 +0200

    * Moved overview graph to '4 Hours' frame

--- icinga-web/inGraph/lib/js/inGraph/ext-inGraph/Ext.iG.TimeFrames.js
+++ icinga-web/inGraph/lib/js/inGraph/ext-inGraph/Ext.iG.TimeFrames.js
@@ -16,8 +16,9 @@ Ext.iG.TimeFrames = Ext.extend(Ext.data.JsonStore, {
 		    data: {
 		       frames: [
 		           {'name': _('One Hour'), start: '-1 hour', enabled: false},
-		           {'name': _('Four Hours'), start: '-4 hours'},
-		           {'name': _('One Day'), start: '-1 day', overview: true},
+		           {'name': _('Four Hours'), start: '-4 hours',
+		            overview: true},
+		           {'name': _('One Day'), start: '-1 day'},
 		           {'name': _('One Week'), start: '-1 week'},
 		           {'name': _('One Month'), start: '-1 month'},
 		           {'name': _('One Year'), start: '-1 year'}

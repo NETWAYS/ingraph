60d8e7a9f987a89b365b09769960356eb65677fa
Eric Lippmann <eric.lippmann@netways.de>
2011-10-12 15:04:14 +0200

    * Removed newlines

--- icinga-web/inGraph/lib/js/inGraph/ext-overrides/String.js
+++ icinga-web/inGraph/lib/js/inGraph/ext-overrides/String.js
@@ -1,23 +1,20 @@
 (function() {
 	var _format = String.format;
-	
 	String.format = function(format) {
 		var newargs = new Array();
-		
 		Ext.each(Ext.toArray(arguments, 1), function(arg) {
 			if(typeof arg == 'object') {
 				for(var key in arg) {
-			        format = format.replace(new RegExp('\\{(' + key + ')\\}', 'gi'), function() {
-			            return arg[key];
+			        format = format.replace(
+			            new RegExp('\\{(' + key + ')\\}', 'gi'), function() {
+			                return arg[key];
 			        });
 				}
 			} else {
 				newargs.push(arg);
 			}
 		});
-		
 		newargs.unshift(format);
-		
 		return _format.apply(this, newargs);
 	};
 })();

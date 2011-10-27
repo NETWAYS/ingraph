e3a647380946f8fd3e0cac34db52a2b7ad4e617d
Eric Lippmann <eric.lippmann@netways.de>
2011-10-27 13:38:09 +0200

    Renamed src directory to ingraph. Renamed netways_ingraph to ingraph. Changed scripts and setup accordingly.

--- setup.py
+++ setup.py
@@ -11,14 +11,13 @@ if sys.version_info[:2] < (2,4):
     print("NETWAYS Grapher requires Python version 2.4 or later.")
     sys.exit(1)
 
-setup(name="netways_ingraph",
+setup(name="ingraph",
       version=version,
       description="Data collection and graphing utility for monitoring systems",
       author="Gunnar Beutner <gunnar.beutner@netways.de>",
       author_email="gunnar.beutner@netways.de",
       url="N/A",
-      packages = ["netways_ingraph"],
-      package_dir = {"netways_ingraph": "src"},
+      packages = ["ingraph"],
       scripts = ["ingraph-daemon", "ingraph-file-collector",
                  "ingraph-import-grapherv2", "ingraph-stop",
                  "ingraph-collectord"],

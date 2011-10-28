10b56f9d8ffad1e62336a508412d3e1240d31ed3
Eric Lippmann <eric.lippmann@netways.de>
2011-10-28 11:37:11 +0200

    Minor changes to daemon and collectord.

--- ingraph/daemon.py
+++ ingraph/daemon.py
@@ -10,8 +10,8 @@ __all__ = ['UnixDaemon']
 
 class UnixDaemon(object):
     def __init__(self, pidfile, umask=0, chdir='/', uid=os.getuid(),
-                 gid=os.getgid(), detach=False, stdin=sys.stdin,
-                 stdout=sys.stdout, stderr=sys.stderr):
+                 gid=os.getgid(), detach=True, stdin=os.devnull,
+                 stdout=os.devnull, stderr=os.devnull):
         self.stdin = stdin
         self.stdout = stdout
         self.stderr = stderr
@@ -23,14 +23,14 @@ class UnixDaemon(object):
         self.umask = umask
         super(UnixDaemon, self).__init__()
 
-    def daemonize(self):
+    def _daemonize(self):
         try: 
             pid = os.fork() 
             if pid > 0:
                 os._exit(0)
         except OSError, e:
-            print >> sys.stderr, "fork #1 failed: %d (%s)" % (e.errno,
-                                                              e.strerror)
+            sys.stderr.write("fork #1 failed: %d (%s)\n" %
+                             (e.errno, e.strerror))
             os._exit(1)
         os.setsid() # os.setpgrp?
         try: 
@@ -38,18 +38,18 @@ class UnixDaemon(object):
             if pid > 0:
                 os._exit(0)
         except OSError, e:
-            print >> sys.stderr, "fork #2 failed: %d (%s)\n" % (e.errno,
-                                                                e.strerror)
+            sys.stderr.write("fork #2 failed: %d (%s)\n" %
+                             (e.errno, e.strerror))
             os._exit(1)
 
-    def _SIGTERM_handler(self, signum, stack_frame):
+    def _SIGTERM(self, signum, stack_frame):
         sys.exit(0)
 
     def _atexit(self):
         self._delpid()
-        self._cleanup()
+        self.cleanup()
 
-    def _cleanup(self):
+    def cleanup(self):
         pass
 
     def _delpid(self):
@@ -77,8 +77,8 @@ class UnixDaemon(object):
     def start(self):
         pid = self._getpid()
         if pid:
-            print >> sys.stderr, "pidfile %s already exists. " \
-                                 "Daemon already running?" % self.pidfile
+            sys.stderr.write("pidfile %s already exists. Daemon already "
+                             "running?\n" % self.pidfile)
             sys.exit(1)
         
         os.umask(self.umask)
@@ -87,29 +87,26 @@ class UnixDaemon(object):
         os.setgid(self.gid)
         
         if self.detach:
-            self.daemonize()
-            
-        self._redirect_stream(sys.stdin, self.stdin)
-        self._redirect_stream(sys.stdout, self.stdout)
-        self._redirect_stream(sys.stderr, self.stderr)
+            self._daemonize()
+            self._redirect_stream(sys.stdin, self.stdin)
+            self._redirect_stream(sys.stdout, self.stdout)
+            self._redirect_stream(sys.stderr, self.stderr)
         
-        pid = str(os.getpid())
-        pf = open(self.pidfile, 'w+b')
-        print >> pf, pid
-        pf.close()
-        
-        signal.signal(signal.SIGTERM, self._SIGTERM_handler)
+        signal.signal(signal.SIGTERM, self._SIGTERM)
         atexit.register(self._atexit)
         
-        self._run()
+        pf = open(self.pidfile, 'w+b')
+        pf.write(str(os.getpid()))
+        pf.close()
+        
+        self.run()
 
     def stop(self):
         pid = self._getpid()
-
         if not pid:
-            print >> sys.stderr, "pidfile %s does not exist. " \
-                                 "Daemon not running?" % self.pidfile
-            return
+            sys.stderr.write("pidfile %s does not exist. Daemon not running?\n"
+                             % self.pidfile)
+            sys.exit(1)
         try:
             os.kill(pid, signal.SIGTERM)
         except OSError, e:
@@ -122,12 +119,12 @@ class UnixDaemon(object):
 
     def status(self):
         pid = self._getpid()
-
         if pid:
-            print "%s daemon running with pid: %i" % (self.__class__.__name__,
-                                                      pid)
+            sys.stdout.write("%s daemon running with pid: %i\n" %
+                             (self.name, pid))
         else:
-            print "%s daemon not running " % self.__class__.__name__
-
-    def _run(self):
+            sys.stdout.write("%s daemon not running\n" %
+                             self.name)
+    
+    def run(self):
         pass

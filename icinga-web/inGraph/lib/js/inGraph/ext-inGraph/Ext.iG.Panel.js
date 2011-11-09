d8ca13083d2e4dfdaf449487c5be8aa45f393d5f
Eric Lippmann <eric.lippmann@netways.de>
2011-11-09 18:48:06 +0100

    Build panels from remote template

--- icinga-web/inGraph/lib/js/inGraph/ext-inGraph/Ext.iG.Panel.js
+++ icinga-web/inGraph/lib/js/inGraph/ext-inGraph/Ext.iG.Panel.js
@@ -21,40 +21,67 @@ Ext.iG.Panel = Ext.extend(Ext.Panel, {
         Ext.iG.Panel.superclass.initComponent.call(this);
     },
     
-    fromViewConfig: function(cfg) {
-        if(this.viewConfig) {
-            var items = [];
-            cfg.title = this.viewConfig.title || _('View (No Title)');
-            Ext.each(this.viewConfig.panels, function(pcfg) {
-                items.push({
-                    titleFormat: '{interval}',
-                    title: pcfg.title || _('Panel (No Title)'),
-                    store: new Ext.iG.FlotJsonStore({
-                        url: this.provider.combined,
-                        baseParams: {
-                            config: Ext.encode({
-                                data: Ext.isArray(pcfg.data) ?
-                                      pcfg.data : new Array(pcfg.data),
-                                flot: this.viewConfig.flot || {},
-                                generic: this.viewConfig.generic || {}
-                            }),
-                            start: pcfg.start ?
-                                   Math.ceil(strtotime(pcfg.start)) : '',
-                            end: pcfg.end ?
-                                 Math.ceil(strtotime(pcfg.end)) :
-                                 Math.ceil(new Date().getTime()/1000),
-                            interval: pcfg.interval || ''
-                        }
-                    })
-                });
-            }, this);
-            cfg.items = items;
-            return true;
-        }
-        return false;
+    initEvents: function() {
+        Ext.iG.Panel.superclass.initEvents.apply(this, arguments);
+        this.addEvents(
+            '__igpanel__complete',
+            '__igpanel__fail'
+        );
     },
     
     fromView: function(cfg) {
+        if(this.view) {
+            var items = [];
+            var callback = function(template) {
+                this.setTitle(template.title || _('View (No Title)'));
+                Ext.each(template.panels, function(panel) {
+                    var query = {};
+                    Ext.each(panel.series, function(series) {
+                        var qpart = query;
+                        Ext.iterate([series.host, series.service],
+                            function(v) {
+                             if(!Ext.isObject(qpart[v])) {
+                                 qpart[v] = {};
+                             }
+                             qpart = qpart[v];
+                        }, this);
+                        if(!Ext.isArray(qpart[series.plot])) {
+                            qpart[series.plot] = [];
+                        }
+                        qpart = qpart[series.plot];
+                        if(qpart.indexOf(series.type) === -1) {
+                            qpart.push(series.type);
+                        }
+                    }, this);
+                    query = Ext.encode(query);
+                    items.push({
+                        titleFormat: '{interval}',
+                        title: panel.title || _('Panel (No Title)'),
+                        template: panel,
+                        store: new Ext.iG.FlotJsonStore({
+                            url: this.provider.values,
+                            baseParams: {
+                                query: query,
+                                start: panel.start ?
+                                       Math.ceil(strtotime(panel.start)) :
+                                       null,
+                                end: panel.end ?
+                                     Math.ceil(strtotime(panel.end)) :
+                                     null,
+                                interval: panel.interval || null
+                            }
+                        })
+                    });
+                }, this);
+                this.add(items);
+                this.doLayout();
+            };
+            var params = {
+                view: this.view
+            };
+            this.requestTemplate(this.provider.view, params, callback);
+            return true;
+        }
         return false;
     },
     
@@ -74,53 +101,61 @@ Ext.iG.Panel = Ext.extend(Ext.Panel, {
         if(this.host && this.service) {
             var items = [];
             cfg.title = this.host + ' - ' + this.service;
-            var query = {}
-            query[this.host] = {};
-            query[this.host][this.service] = {};
-            Ext.each(this.template.series, function(series) {
-                query[this.host][this.service][series.plot] = Ext.isArray(
-                    series.type) ? series.type : [series.type];
-            }, this);
-            query = Ext.encode(query);
-            if(this.start || this.end) {
-                items.push({
-                    title: this.start + ' - ' + this.end,
-                    host: this.host,
-                    service: this.service,
-                    template: this.template,
-                    store: new Ext.iG.FlotJsonStore({
-                        url: this.provider.values,
-                        baseParams: {
-                            query: query,
-                            start: this.start,
-                            end: this.end
-                        }
-                    })
-                });
-            } else {
-                this.timeFrames.each(function(rec) {
-                    if(rec.get('enabled')) {
-                        items.push({
-                            title: rec.get('name'),
-                            activeFrame: rec.get('name'),
-                            host: this.host,
-                            service: this.service,
-                            template: this.template,
-                            store: new Ext.iG.FlotJsonStore({
-                                url: this.provider.values,
-                                baseParams: {
-                                    query: query,
-                                    start: Math.ceil(
-                                        strtotime(rec.get('start'))),
-                                    end: Math.ceil(strtotime(rec.get('end')))
-                                }
-                            }),
-                            overview: rec.get('overview')
-                        });
-                    }
+            var callback = function(template) {
+                var query = {}
+                query[this.host] = {};
+                query[this.host][this.service] = {};
+                Ext.each(template.series, function(series) {
+                    query[this.host][this.service][series.plot] = Ext.isArray(
+                        series.type) ? series.type : [series.type];
                 }, this);
-            }
-            cfg.items = items;
+                query = Ext.encode(query);
+                if(this.start || this.end) {
+                    items.push({
+                        title: this.start + ' - ' + this.end,
+                        host: this.host,
+                        service: this.service,
+                        template: template,
+                        store: new Ext.iG.FlotJsonStore({
+                            url: this.provider.values,
+                            baseParams: {
+                                query: query,
+                                start: this.start,
+                                end: this.end
+                            }
+                        })
+                    });
+                } else {
+                    this.timeFrames.each(function(rec) {
+                        if(rec.get('enabled')) {
+                            items.push({
+                                title: rec.get('name'),
+                                activeFrame: rec.get('name'),
+                                host: this.host,
+                                service: this.service,
+                                template: template,
+                                store: new Ext.iG.FlotJsonStore({
+                                    url: this.provider.values,
+                                    baseParams: {
+                                        query: query,
+                                        start: Math.ceil(
+                                            strtotime(rec.get('start'))),
+                                        end: Math.ceil(strtotime(rec.get('end')))
+                                    }
+                                }),
+                                overview: rec.get('overview')
+                            });
+                        }
+                    }, this);
+                }
+                this.add(items);
+                this.doLayout();
+            };
+            var params = {
+                host: this.host,
+                service: this.service
+            };
+            this.requestTemplate(this.provider.template, params, callback);
             return true;
         }
         return false;
@@ -128,8 +163,8 @@ Ext.iG.Panel = Ext.extend(Ext.Panel, {
     
     buildItems: function(cfg) {
         var items = [];
-        Ext.each([this.fromViewConfig, this.fromView, this.fromHost,
-                  this.fromHostService], function(fn) {
+        Ext.each([this.fromHostService, this.fromView, this.fromHost],
+                 function(fn) {
             return !fn.call(this, cfg);
         }, this);
     },
@@ -151,5 +186,24 @@ Ext.iG.Panel = Ext.extend(Ext.Panel, {
             }
             this.add(panel);
         }, this);
+    },
+    
+    requestTemplate: function(url, params, callback) {
+        Ext.Ajax.request({
+           url: url,
+           scope: this,
+           success: function(res) {
+               var template = null;
+               if(res.responseText) {
+                   template = Ext.decode(res.responseText);
+               }
+               callback.call(this, template);
+               this.fireEvent('__igpanel__complete', this);
+           },
+           failure: function() {
+               this.fireEvent('__igpanel__fail', this, arguments);
+           },
+           params: params
+        });
     }
 });

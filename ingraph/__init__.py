afcc34f420730a41ae37c1e6d936cc2064a39d1e
Gunnar Beutner <gunnar.beutner@netways.de>
2011-11-28 15:43:50 +0100

    Updated license information.

--- ingraph/__init__.py
+++ ingraph/__init__.py
@@ -1,5 +1,21 @@
+# inGraph (https://www.netways.org/projects/ingraph)
+# Copyright (C) 2011 NETWAYS GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 __name__ = 'ingraph'
-__version__ = '1.0 Alpha'
+__version__ = '1.0'
 __author__ = 'NETWAYS GmbH'
 __contact__ = 'info@netways.de'
 __url__ = 'https://www.netways.org'

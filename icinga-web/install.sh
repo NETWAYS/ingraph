158acc05fe997f61fd90b51396f803ad7f740414
Eric Lippmann <eric.lippmann@netways.de>
2011-12-05 00:07:20 +0100

    Improve installer

--- icinga-web/install.sh
+++ icinga-web/install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-set -e
-
 SCRIPT=`readlink -f $0`
 DIR=`dirname $SCRIPT`
 PREFIX=/usr/local/icinga-web
@@ -34,4 +32,27 @@ do
     esac
 done
 
+ICINGA_XML=$PREFIX/app/config/icinga.xml
+[ ! -r "$ICINGA_XML" ] && {
+    echo "No icinga-web installation found."
+    exit 1
+}
+
+VERSION=`grep \<setting\ name=\"version.minor\"\> $ICINGA_XML | tr -dc [0-9]`
+[ $VERSION -lt 5 ] && {
+    echo "Icinga-web versions older than 1.5.0 are currently not supported."
+    exit 1
+}
+
+[ $VERSION -lt 6 ] && {
+    patch --dry-run $PREFIX/app/modules/Cronks/lib/template/CronkGridTemplateXmlParser.class.php icinga-web-1.5.x.patch > /dev/null
+    [ $? -eq 0 ] && {
+        patch $PREFIX/app/modules/Cronks/lib/template/CronkGridTemplateXmlParser.class.php icinga-web-1.5.x.patch
+    } || {
+        echo "WARNING: Unable to apply icinga-web patch. Continuing anyway."
+    }
+}
+
 cp -r $DIR/inGraph $PREFIX/app/modules/
+
+exit 0

f78a7a5d3179ae4b292b2a3bb33ce038b51555d6
Eric Lippmann <eric.lippmann@netways.de>
2011-12-05 00:18:33 +0100

    Fix getBaseUrl replacement

--- web/inGraph/pub/js/extra.js
+++ web/inGraph/pub/js/extra.js
@@ -3,7 +3,9 @@ Ext.ns('AppKit.util.Config');
 AppKit.util.Config = function() {
     return {
         getBaseUrl: function() {
-            return '/ingraph';
+            return (document.location.protocol.indexOf('https') === 0 ? 'https://' : 'http://') +
+                   (document.location.host || document.domain) +
+                   document.location.pathname.replace(/\/$/, '');
         }
     };
 }();

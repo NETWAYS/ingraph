bf17f0906201a5c74c26742a56995c33b7bf5275
Eric Lippmann <eric.lippmann@netways.de>
2011-12-05 16:57:22 +0100

    Do not use AppKitAgaviUtil::log

--- icinga-web/inGraph/lib/model/inGraphBaseModel.class.php
+++ icinga-web/inGraph/lib/model/inGraphBaseModel.class.php
@@ -6,5 +6,6 @@ class inGraphBaseModel extends AppKitBaseModel {
     public function initialize(AgaviContext $ctx, array $params = array()) {
         parent::initialize($ctx, $params);
         $this->tm = $this->getContext()->getTranslationManager();
+        $this->logger = $this->getContext()->getLoggerManager();
     }
 }

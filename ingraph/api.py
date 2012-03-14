3d6a3e78cc7ea007c6b3743b41c436c78fda7d9e
Gunnar Beutner <gunnar.beutner@netways.de>
2012-03-14 13:10:15 +0100

    Updated copyright.

--- ingraph/api.py
+++ ingraph/api.py
@@ -1,5 +1,5 @@
 # inGraph (https://www.netways.org/projects/ingraph)
-# Copyright (C) 2011 NETWAYS GmbH
+# Copyright (C) 2011-2012 NETWAYS GmbH
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

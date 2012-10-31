e4159f93f85f79c00a8fe3b5ef6bdae38f044a99
Eric Lippmann <eric.lippmann@netways.de>
2012-10-31 13:17:42 +0100

    api/getPlots: Cover parent service
    refs #1598

--- ingraph/api.py
+++ ingraph/api.py
@@ -389,38 +389,63 @@ class BackendRPCMethods(object):
         return self.addOrUpdateComment(comment_id, host, parent_service,
             service, timestamp, author, text)
         
-    def getPlots(self, host_name, service_name):
+    def getPlots(self, host_name, service_name, parent_service_name=None):
         res = []
         host = model.Host.getByName(self.engine, host_name)
         if host_name and not host:
             return res
-        service = model.Service.getByName(self.engine, service_name)
+        service = model.Service.getByName(self.engine, service_name,
+                                          parent_service_name)
         if service_name and not service:
             return res
+        parent_hose = None
+        if parent_service_name:
+            parent_service = model.Service.getByName(
+                self.engine, parent_service_name, None)
+            if not parent_service:
+                return res
+            parent_hose = model.HostService.getByHostAndService(
+                self.engine, host, parent_service, None)
+            try:
+                parent_hose = parent_hose[0]
+            except IndexError:
+                # Not found
+                pass
         hose = model.HostService.getByHostAndService(
-            self.engine, host, service, None)
+            self.engine, host, service, parent_hose)
         try:
             hose = hose[0]
         except IndexError:
+            # Not found
             pass
         else:
             children = model.HostService.getByHostAndService(
                 self.engine, hose.host, None, hose)
             if children:
                 for child in children:
+                    if child.parent_hostservice != None:
+                        parent_service_name = child.parent_hostservice.service.name
+                    else:
+                        parent_service_name = None
                     plots = model.Plot.getByHostServiceAndName(
                         self.engine, child, None)
                     for plot in plots:
                         res.append({
                             'service': child.service.name,
-                            'plot': plot.name
+                            'plot': plot.name,
+                            'parent_service': parent_service_name
                         })
             else:
+                if hose.parent_hostservice != None:
+                    parent_service_name = hose.parent_hostservice.service.name
+                else:
+                    parent_service_name = None
                 plots = model.Plot.getByHostServiceAndName(
                     self.engine, hose, None)
                 for plot in plots:
                     res.append({
                         'service': hose.service.name,
-                        'plot': plot.name
+                        'plot': plot.name,
+                        'parent_service': parent_service_name
                     })
         return res

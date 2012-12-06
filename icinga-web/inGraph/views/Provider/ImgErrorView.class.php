48c05a398ed5fce9a8538c7ad583a062dc9bde6c
Eric Lippmann <eric.lippmann@netways.de>
2012-12-06 15:33:57 +0100

    Provider/Img: Update views

--- icinga-web/inGraph/views/Provider/ImgErrorView.class.php
+++ icinga-web/inGraph/views/Provider/ImgErrorView.class.php
@@ -1,9 +1,10 @@
 <?php
 
-class inGraph_Provider_ImgErrorView extends inGraphBaseView {
-    public function executeImage(AgaviRequestDataHolder $rd) {
-        header('Content-type: text/html');
-        echo $this->getAttribute('errorMessage');
-        exit;
+class inGraph_Provider_ImgErrorView extends inGraphBaseView
+{
+    public function executeImage(AgaviRequestDataHolder $rd)
+    {
+        $response = $this->getContainer()->getResponse();
+        $response->setHttpHeader('Content-Type', 'text/html');
     }
 }
--- icinga-web/inGraph/views/Provider/ImgSuccessView.class.php
+++ icinga-web/inGraph/views/Provider/ImgSuccessView.class.php
@@ -5,11 +5,13 @@ class inGraph_Provider_ImgSuccessView extends inGraphBaseView
 	public function executeImage(AgaviRequestDataHolder $rd)
     {
         $params = $this->getAttribute('params');
-
-        $node = NodeJs::getInstance();
-        
-        $res = $node->renderImage($params);
-
-        return $res;
+        $nodeJsConfig = AgaviConfig::get('modules.ingraph.nodejs');
+        $node = inGraph_Renderer_NodeJs::getInstance(array(
+            'cmd' => $nodeJsConfig['bin'] . ' ' 
+                . AgaviConfig::get('core.module_dir') 
+                . '/inGraph/lib/nodejs/flot-renderer.js'
+        ));
+        $image = $node->renderImage($params);
+        return $image;
 	}
 }

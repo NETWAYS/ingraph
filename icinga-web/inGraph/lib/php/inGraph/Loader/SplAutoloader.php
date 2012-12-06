63846e9c02c3d6dbfae7d7a1f121fdc6dd3f38a5
Eric Lippmann <eric.lippmann@netways.de>
2012-12-06 15:35:57 +0100

    Remove license headers

--- icinga-web/inGraph/lib/php/inGraph/Loader/SplAutoloader.php
+++ icinga-web/inGraph/lib/php/inGraph/Loader/SplAutoloader.php
@@ -1,29 +1,4 @@
- <?php
- /**
-  * SplAutoloader.php
-  *
-  * Copyright (C) 2012 NETWAYS GmbH, http://netways.de
-  *
-  * This file is part of inGraph.
-  *
-  * inGraph is free software: you can redistribute it and/or modify it under
-  * the terms of the GNU General Public License as published by the Free Software
-  * Foundation, either version 3 of the License, or any later version.
-  *
-  * inGraph is distributed in the hope that it will be useful, but WITHOUT
-  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-  * details.
-  *
-  * You should have received a copy of the GNU General Public License along with
-  * inGraph. If not, see <http://www.gnu.org/licenses/gpl.html>.
-  *
-  * @link https://www.netways.org/projects/ingraph
-  * @author Eric Lippmann <eric.lippmann@netways.de>
-  * @copyright Copyright (c) 2012 NETWAYS GmbH (http.netways.de) <info@netways.de>
-  * @license http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License
-  * @package inGraph_Loader
-  */
+<?php
 
 final class inGraph_Loader_SplAutoloader
 {
--- icinga-web/inGraph/lib/php/inGraph/Os/Process.php
+++ icinga-web/inGraph/lib/php/inGraph/Os/Process.php
@@ -1,29 +1,4 @@
 <?php
-/**
- * Process.php
- *
- * Copyright (C) 2012 NETWAYS GmbH, http://netways.de
- *
- * This file is part of inGraph.
- *
- * inGraph is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or any later version.
- *
- * inGraph is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * inGraph. If not, see <http://www.gnu.org/licenses/gpl.html>.
- *
- * @link https://www.netways.org/projects/ingraph
- * @author Eric Lippmann <eric.lippmann@netways.de>
- * @copyright Copyright (c) 2012 NETWAYS GmbH (http.netways.de) <info@netways.de>
- * @license http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License
- * @package inGraph_Os
- */
 
 class inGraph_Os_Process extends ArrayObject
 {
@@ -77,21 +52,21 @@ class inGraph_Os_Process extends ArrayObject
 
     protected function getPipes()
     {
-        if (null === $this->_resource) {
+        if (null === $this->resource) {
             $descriptorspec = array(
                 0 => array('pipe', 'r'), // stdin
                 1 => array('pipe', 'w'), // stdout
                 2 => array('pipe', 'w')  // stderr
             );
-            $this->_resource = proc_open(
+            $this->resource = proc_open(
                 $this->cmd,
                 $descriptorspec,
-                $this->_pipes,
+                $this->pipes,
                 $this->cwd,
                 // Environment defaults to current PHP process' environment
                 null
             );
-            if (!is_resource($this->_resource)) {
+            if (!is_resource($this->resource)) {
                 throw new Exception(sprintf(
                     'Cannot fork "%s"',
                     $this->cmd
@@ -99,28 +74,28 @@ class inGraph_Os_Process extends ArrayObject
             }
         }
         return (object) array(
-            'stdin' => & $this->_pipes[0],
-            'stdout' => & $this->_pipes[1],
-            'stderr' => & $this->_pipes[2]
+            'stdin' => & $this->pipes[0],
+            'stdout' => & $this->pipes[1],
+            'stderr' => & $this->pipes[2]
         );
     }
 
     public function getStatus()
     {
-        $status = (object) proc_get_status($this->_resource);
+        $status = (object) proc_get_status($this->resource);
         if (false === $status->running
-            && null === $this->_exitCode
+            && null === $this->exitCode
         ) {
             // The exit code is only valid the first time proc_get_status is
             // called in terms of running false, hence we capture it
-            $this->_exitCode = $status->exitcode;
+            $this->exitCode = $status->exitcode;
         }
         return $status;
     }
 
     public function close()
     {
-        if (null === $this->_resource){
+        if (null === $this->resource){
             return 0;
         }
         $pipes = $this->getPipes();
@@ -129,18 +104,18 @@ class inGraph_Os_Process extends ArrayObject
         }
         fclose($pipes->stdout);
         fclose($pipes->stderr);
-        if (null === $this->_exitCode) {
-            $this->_exitCode = proc_close($this->_resource);
+        if (null === $this->exitCode) {
+            $this->exitCode = proc_close($this->resource);
         }
-        $this->_resource = null;
-        if (0 !== $this->_exitCode) {
+        $this->resource = null;
+        if (0 !== $this->exitCode) {
             throw new Exception(sprintf(
                 'Running "%s" failed with exit code (%d)',
                 $this->cmd,
-                $this->_exitCode
+                $this->exitCode
             ));
         }
-        return $this->_exitCode;
+        return $this->exitCode;
     }
 
     public function __destruct()

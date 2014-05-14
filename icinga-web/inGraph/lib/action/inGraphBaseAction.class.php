85dca6aaf90b6902ead24a6f6c3389cae003aba3
Eric Lippmann <eric.lippmann@netways.de>
2014-05-14 16:47:29 +0200

    perms: Temporary grant `ingraph.view.modify' to all users

--- icinga-web/inGraph/lib/action/inGraphBaseAction.class.php
+++ icinga-web/inGraph/lib/action/inGraphBaseAction.class.php
@@ -45,6 +45,7 @@ class inGraphBaseAction extends IcingaBaseAction
 
     public function executeWrite(AgaviRequestDataHolder $rd)
     {
+        $this->context->getUser()->addCredential('ingraph.view.modify');
         $this->setAttribute('credentials', $this->context->getUser()->getCredentials());
         return $this->getDefaultViewName();
     }

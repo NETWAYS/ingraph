7fcf5298e3621372e42c6d6e8d291895cf2d5258
Eric Lippmann <eric.lippmann@netways.de>
2014-07-16 09:21:25 +0200

    Graphite: Allow static metrics in `target'

--- icinga-web/inGraph/lib/php/inGraph/Backend/Graphite.php
+++ icinga-web/inGraph/lib/php/inGraph/Backend/Graphite.php
@@ -34,11 +34,11 @@ class inGraph_Backend_Graphite extends Graphite implements inGraph_Backend
             0,
             strpos($config['namingScheme'], '<metric>')
         ) . '<metric>';
-        $this->pattern = sprintf('/%s/',
+        $this->pattern = sprintf('/%s(?:\.(?P<type>[[:word:]-]+))?/',
             str_replace(
                 array('<host>', '<service>', '<metric>'),
                 array('(?P<host>[[:word:]-]+)', '(?P<service>[[:word:]-]+)', '(?P<metric>[[:word:]-]+)'),
-                $this->metricFormat
+                addcslashes($this->metricFormat, '.')
             )
         );
         $this->staticMetricsPath = $config['staticMetricsPath'];
@@ -120,45 +120,68 @@ class inGraph_Backend_Graphite extends Graphite implements inGraph_Backend
         );
     }
 
+    public function fetchStaticMetric($host, $service, $plot, $type)
+    {
+        $jsonPath = $this->staticMetricsPath . '/' . base64_encode($host)
+            . ($service === '' ? '' : '/' . base64_encode($service))
+            . '/' . base64_encode($plot) . '.json';
+        if (! is_readable($jsonPath)) {
+            return null;
+        }
+        if (! array_key_exists($host, $this->metricsCache)) {
+            $this->metricsCache[$host] = array();
+        }
+        if (! array_key_exists($service, $this->metricsCache[$host])) {
+            $this->metricsCache[$host][$service] = array();
+        }
+        if (! array_key_exists($plot, $this->metricsCache[$host][$service])) {
+            $this->metricsCache[$host][$service][$plot] = json_decode(
+                file_get_contents($jsonPath), true
+            );
+        }
+        $metric = array();
+        $jsonData = $this->metricsCache[$host][$service][$plot];
+        foreach ($jsonData as $jsonMetric) {
+            $metric[] = array($jsonMetric['timestamp'], $jsonMetric[$type]);
+        }
+        return $metric;
+    }
+
     public function fetchValues($query, $start = null, $end = null)
     {
         $charts = array();
         $comments = array();
         foreach ($query as $spec) {
-            if ($spec['type'] !== 'avg') {
-                $data = array();
-                $jsonPath = $this->staticMetricsPath . '/' . base64_encode($spec['host'])
-                    . ($spec['service'] === '' ? '' : '/' . base64_encode($spec['service']))
-                    . '/' . base64_encode($spec['plot']) . '.json';
-                if (!array_key_exists($spec['host'], $this->metricsCache)) {
-                    $this->metricsCache[$spec['host']] = array();
-                }
-                if (!array_key_exists($spec['service'], $this->metricsCache[$spec['host']])) {
-                    $this->metricsCache[$spec['host']][$spec['service']] = array();
-                }
-                if (!array_key_exists($spec['plot'], $this->metricsCache[$spec['host']][$spec['service']])) {
-                    if (is_readable($jsonPath)) {
-                        $this->metricsCache[$spec['host']][$spec['service']][$spec['plot']] = json_decode(
-                            file_get_contents($jsonPath), true
-                        );
-                    } else {
-                        continue;
-                    }
-                }
-                $jsonData = $this->metricsCache[$spec['host']][$spec['service']][$spec['plot']];
-                foreach ($jsonData as $jsonMetric) {
-                    $data[] = array($jsonMetric['timestamp'], $jsonMetric[$spec['type']]);
-                }
-                $charts[] = array(
-                    'label' => $spec['plot'] . ' ' . $spec['type'],
-                    'data'  => $data
-                ) + $spec;
-                continue;
-            }
             if (isset($spec['target'])) {
-                $plotId = null;
-                $target = $spec['target'];
+                $plotId = md5($spec['target']);
+                $self = $this;
+                $target = preg_replace_callback(
+                    $this->pattern,
+                    function ($match) use ($self) {
+                        if (array_key_exists('type', $match)) {
+                            $metric = $self->fetchStaticMetric(
+                                $match['host'], $match['service'], $match['metric'], $match['type']
+                            );
+                            if ($metric !== null) {
+                                $metric = end($metric);
+                                return $metric[1];
+                            }
+                        }
+                        return $match[0];
+                    },
+                    $spec['target']
+                );
             } else {
+                if ($spec['type'] !== 'avg') {
+                    $metric = $this->fetchStaticMetric($spec['host'], $spec['service'], $spec['plot'], $spec['type']);
+                    if ($metric !== null) {
+                        $charts[] = array(
+                                'label' => $spec['plot'] . ' ' . $spec['type'],
+                                'data'  => $metric
+                            ) + $spec;
+                    }
+                    continue;
+                }
                 $plotId = sprintf(
                     '%s - %s - %s - %s', $spec['host'], $spec['service'], $spec['plot'], $spec['type']);
                 $target = sprintf(
@@ -185,7 +208,7 @@ class inGraph_Backend_Graphite extends Graphite implements inGraph_Backend
                     continue;
                 }
                 $lastDatapoint = end($metric['datapoints']);
-                if (!isset($spec['target'])) {
+                if (! isset($spec['target'])) {
                     $comments = array_merge(
                         $comments,
                         $this->fetchComments($spec['host'], $spec['service'], $firstDatapoint[1], $lastDatapoint[1])
@@ -222,7 +245,7 @@ class inGraph_Backend_Graphite extends Graphite implements inGraph_Backend
         $conn = AgaviContext::getInstance()->getDatabaseManager()->getDatabase('ingraph')->getConnection();
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $conn->prepare(
-            'INSERT INTO comment (host, service, time, author, text) VALUES (:host, :service, FROM_UNIXTIME(:time),'
+            'INSERT INTO `comment` (host, service, time, author, text) VALUES (:host, :service, FROM_UNIXTIME(:time),'
             . ' :author, :text)'
         );
         $stmt->execute(array(
